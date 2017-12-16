e43fdd4a9bc369bfaaa0c2919fffa9c4e544140b
MG08 <miguel_itecit@hotmail.com>
2017-12-15 23:54:16 -0600

    r0.9-1

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:iKKgenJV4gWgsYBT8dsbpU3zb3xnnL4x+N5lTYRrCC0='),
 
     'cipher' => 'AES-256-CBC',
 
